8010ec9830477b719034124d71470834ba2b2524
marbsostique12 <majenzenos@gmail.com>
2024-07-27 11:57:08 +1200

    My comment in your work

--- LearnC#/Program.cs
+++ LearnC#/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World this is the modified file!");
 Console.WriteLine("This is the work of another programmer!");
+Console.WriteLine("Looks good to me");
