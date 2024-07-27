133b72ebd37ed6768edc6f663ff180b801e58cd3
marbsostique12 <majenzenos@gmail.com>
2024-07-27 12:25:49 +1200

    Last modification for master

--- LearnC#/Program.cs
+++ LearnC#/Program.cs
@@ -1,12 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-static void sayHello(){
-    Console.WriteLine("Hello");
-}
-static void sayHi()
-{
-    Console.WriteLine("Hello");
-}
-static void sayHey()
-{
-    Console.WriteLine("Hello");
-}
+Console.WriteLine("Hello");
